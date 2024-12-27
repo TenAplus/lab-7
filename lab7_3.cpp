a4f274f041e4113e02a63d1045e89e8db77c9199
TenAplus <pongthitheethawat@gmail.com>
2024-12-27 19:26:09 +0700

    Update lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if (x=='A'){
+		char x = 'Z' ;
+		return x;
+	}
+	if( x > 'A' && x <= 'Z'){
+        char y =  x - 1;
+        return y;
+        }
+    else{
+        char x = '0';
+		return x;
+        }
 }
 
 int main(){
